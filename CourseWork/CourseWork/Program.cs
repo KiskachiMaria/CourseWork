b1140d504b87a35a4cc1f9d7f55b4d0c143ca381
Kima <angele_ya@mail.ru>
2014-03-18 14:16:48 +0600

    Start.

---
+++ CourseWork/CourseWork/Program.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace CourseWork
+{
+	public static class Converter
+	{
+		public static void ConvertStandartToRegions(string fnameIn, string fnameOut)
+		{
+			var fin = new StreamReader(fnameIn);
+			var fout = new StreamWriter(fnameOut);
+			const int bufferSize = 5000;
+			var buff = new char[bufferSize];
+			string residual = "";
+
+			string lastPerson = "";
+			var adjacentRegions = new List<string>();
+	
+			while(!fin.EndOfStream)
+			{
+				fin.ReadBlock(buff, 0, bufferSize);
+
+				var buffer = new string(buff);
+				var strs = new List<string>();
+
+				if(residual != "" && residual[residual.Length - 1] == ']')
+					strs.Add(residual);
+				strs.AddRange(buffer.Split(new[] { ';', '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries));
+				if(residual != "" && residual[residual.Length - 1] != ']')
+					strs[0] = residual + strs[0];
+				residual = strs.Last();
+
+				string bufferOut = "";
+
+				for(int i = 0; i < strs.Count - 1; i++)
+				{
+					string[] edge = strs[i].Split(new[] { ' ', '-', '>' }, StringSplitOptions.RemoveEmptyEntries);
+					if(edge.Count() != 3 || edge[2][3] == '4')
+						continue;
+					if(edge[0][0] == 'f')
+					{
+						bufferOut += edge[0] + " -> " + edge[1] + "	" + "1\r\n";
+					}
+					if(edge[0][0] == 'p')
+					{
+						if(lastPerson != edge[0])
+						{
+							for(var regIn = 0; regIn < adjacentRegions.Count; regIn++)
+								for(var regOut = regIn + 1; regOut < adjacentRegions.Count; regOut++)
+								{
+									bufferOut += adjacentRegions[regIn] + " -> " + adjacentRegions[regOut] + "	" + 1.0 / (adjacentRegions.Count - 1.0) + "\r\n";
+									bufferOut += adjacentRegions[regOut] + " -> " + adjacentRegions[regIn] + "	" + 1.0 / (adjacentRegions.Count - 1.0) + "\r\n";
+								}
+							adjacentRegions = new List<string>();
+						}
+						adjacentRegions.Add(edge[1]);
+					}
+					lastPerson = edge[0];
+				}
+				fout.Write(bufferOut);
+			}
+
+			fin.Close();
+			fout.Close();
+		}
+	}
+
+	static class Program
+	{
+		static void Main()
+		{
+			var seq = new Sequences("BG.all.v2.sort");
+			seq.Counting();
+			seq.WriteInfo();
+
+		}
+
+		private static void CountSumK()
+		{
+			var ss = new Sequences("BG.all.v2.sort");
+			var strs = File.ReadAllLines("BG.edges")
+				.GroupBy(x => ss.RightRegion(x.Substring(4, 2)))
+				.Select(x =>
+					new
+					{
+						reg = x.Key,
+						sumK = x.GroupBy(y => y.Substring(4, 4))
+					.Select(g => Math.Pow(g.Count(), 2))
+					.Sum()
+					})
+					.Select(q => q.reg + "	" + q.sumK);
+			File.WriteAllLines("Inspect.Rigth", strs);
+		}
+
+		private static void Old()
+		{
+			Converter.ConvertStandartToRegions("BigGraph", "Big_Graph_Region_v3");
+			Console.WriteLine("Convert to region fineshed");
+			return;
+			var fin = new StreamReader("Big_Graph_Region_v3");
+			var fout = new StreamWriter("Debug_print");
+			var foutData = new StreamWriter("data.gv");
+
+			var dict = new double[9999][];
+
+			for(int i = 0; i < 9999; i++)
+				dict[i] = new double[9999];
+
+
+			while(!fin.EndOfStream)
+			{
+				var readLine = fin.ReadLine();
+				if(readLine == null) continue;
+				var s = readLine.Split(new[] { '-', '>', '	' }, StringSplitOptions.RemoveEmptyEntries);
+				int v1, v2;
+				double v3;
+				Int32.TryParse(s[0], out v1);
+				Int32.TryParse(s[1], out v2);
+				Double.TryParse(s[2], out v3);
+				dict[v1][v2] += v3;
+			}
+
+			for(int i = 1; i < 9999; i++)
+			{
+				double max = 0.0;
+				int ind = -1;
+				string s = "";
+				for(int j = 1; j < 9999; j++)
+				{
+					if(dict[i][j] > max)
+					{
+						max = dict[i][j];
+						ind = j;
+					}
+					if(Math.Abs(dict[i][j] - 0) > 0.00000000000000000000001)
+						s += dict[i][j] + "  ";
+				}
+
+				fout.WriteLine(i + "(" + ind + ") : " + s);
+			}
+
+			for(var i = 1; i < 9999; i++)
+			{
+				for(var j = 1; j < 9999; j++)
+				{
+					if(Math.Abs(dict[i][j] - 0) > 0.00000000000000000000001)
+						foutData.WriteLine(i + "--" + j + "[weight=" + dict[i][j] + "];");
+				}
+			}
+
+			fin.Close();
+			fout.Close();
+			foutData.Close();
+		}
+	}
+}
---
+++ CourseWork/CourseWork/Sequences.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+
+namespace CourseWork
+{
+	class Sequences
+	{
+		private readonly string fname;
+		private int insertInsp;
+		private int insertReg;
+		private int insertContr;
+		private double doubleInsertInsp;
+		private double doubleInsertReg;
+		private double doubleInsertCountr;
+		private int longerInsertInsp;
+		private int longerInsertReg;
+		private int longerInsertCountr;
+
+		private readonly Dictionary<string, int> regionsInsp;
+		private readonly Dictionary<string, int> regionsReg;
+		private readonly Dictionary<string, int> regionsCou;
+		private readonly Dictionary<string, double> doubleRegionsInsp;
+		private readonly Dictionary<string, double> doubleRegionsReg;
+		private readonly Dictionary<string, double> doubleRegionsCou;
+		private readonly Dictionary<string, int> longerRegionInsp;
+		private readonly Dictionary<string, int> longerRegionReg;
+		
+		public Sequences(string fname)
+		{
+			this.fname = fname;
+			regionsInsp = new Dictionary<string, int>();
+			regionsReg = new Dictionary<string, int>();
+			regionsCou = new Dictionary<string, int>();
+			doubleRegionsInsp = new Dictionary<string, double>();
+			doubleRegionsReg = new Dictionary<string, double>();
+			doubleRegionsCou = new Dictionary<string, double>();
+			longerRegionInsp = new Dictionary<string, int>();
+			longerRegionReg = new Dictionary<string, int>();
+		}
+
+		public string RightRegion (string reg)
+		{
+			if(reg == "81")
+				return "59";
+			if(reg == "84" || reg == "88")
+				return "24";
+			if(reg == "82")
+				return "41";
+			if(reg == "85")
+				return "38";
+			if(reg == "80")
+				return "75";
+			if(reg == "96")
+				return "66";
+			if(reg == "98")
+				return "78";
+			if(reg == "50")
+				return "77";
+			if(reg == "47")
+				return "78";
+//			Питер и Москва с облостями
+			return reg;
+		}
+
+		public void Counting()
+		{
+			using(var reader = new StreamReader(File.OpenRead(fname)))
+			{
+				string str;
+				string lastOrg = "";
+				var links = new List<string>();
+				while((str = reader.ReadLine()) != null)
+				{
+					if(String.IsNullOrWhiteSpace(str))
+						continue;
+					var edge = str.Split(new[] { ' ', '-', '>', ' ', '	' }, StringSplitOptions.RemoveEmptyEntries);
+					if(edge.Count() != 3)
+						continue;
+
+					if(lastOrg != edge[0])
+					{
+						var regionFrom = "";
+						var regionTo = "";
+
+						foreach(var link in links)
+						{
+							regionFrom = RightRegion(lastOrg.Substring(4, 2));
+							regionTo = RightRegion(link.Substring(4, 2));
+								
+							double dummy;
+							double.TryParse(link.Substring(15), NumberStyles.Any, CultureInfo.InvariantCulture, out dummy);
+							if(lastOrg.Substring(4, 4) == link.Substring(4, 4))//одинаковые инспекции
+							{
+								insertInsp++;
+								doubleInsertInsp += dummy;
+								if(!regionsInsp.ContainsKey(regionFrom))
+									regionsInsp[regionFrom] = 0;
+								regionsInsp[regionFrom]++;
+								if(!doubleRegionsInsp.ContainsKey(regionFrom))
+									doubleRegionsInsp[regionFrom] = 0;
+								doubleRegionsInsp[regionFrom] += dummy;
+							}
+							if(regionFrom == regionTo)//одинаковые регионы
+							{
+								insertReg++;
+								doubleInsertReg += dummy;
+								if(!regionsReg.ContainsKey(regionFrom))
+									regionsReg[regionFrom] = 0;
+								regionsReg[regionFrom]++;
+								if(!doubleRegionsReg.ContainsKey(regionFrom))
+									doubleRegionsReg[regionFrom] = 0;
+								doubleRegionsReg[regionFrom] += dummy;
+							}
+							else
+							{
+								if(!regionsCou.ContainsKey(regionFrom))
+									regionsCou[regionFrom] = 0;
+								regionsCou[regionFrom]++;
+								if(!doubleRegionsCou.ContainsKey(regionFrom))
+									doubleRegionsCou[regionFrom] = 0;
+								doubleRegionsCou[regionFrom] += dummy;
+//								if(!regionsCou.ContainsKey(regionTo))
+//									regionsCou[regionTo] = 0;
+//								regionsCou[regionTo]++;
+//								if(!doubleRegionsCou.ContainsKey(regionTo))
+//									doubleRegionsCou[regionTo] = 0;
+//								doubleRegionsCou[regionTo] += dummy;
+							}
+							insertContr++;
+							doubleInsertCountr += dummy;
+						}
+
+						bool ins = false, reg = false, cou = false;
+						foreach(var link in links)
+						{
+							double dummy;
+							double.TryParse(link.Substring(15), NumberStyles.Any, CultureInfo.InvariantCulture, out dummy);
+							if(lastOrg.Substring(4, 4) == link.Substring(4, 4))
+								ins = true;
+							else if(regionFrom == regionTo)
+								reg = true;
+							else
+								cou = true;
+							
+							if(cou)
+								longerInsertCountr++;
+							else if(reg)
+							{
+								longerInsertCountr++;
+								longerInsertReg++;
+								if(!longerRegionReg.ContainsKey(regionFrom))
+									longerRegionReg[regionFrom] = 0;
+								longerRegionReg[regionFrom]++;
+							}
+							else if(ins)
+							{
+								longerInsertCountr++;
+								longerInsertInsp++;
+								longerInsertReg++;
+								if(!longerRegionInsp.ContainsKey(regionFrom))
+									longerRegionInsp[regionFrom] = 0;
+								if(!longerRegionReg.ContainsKey(regionFrom))
+									longerRegionReg[regionFrom] = 0;
+								longerRegionReg[regionFrom]++;
+								longerRegionInsp[regionFrom]++;
+							}
+						}
+
+						links = new List<string>();
+					}
+
+					links.Add(edge[1] + "	" + edge[2]);
+					lastOrg = edge[0];
+					
+				}
+			}
+		}
+
+		public void WriteInfo()
+		{
+			Console.WriteLine(insertInsp + " " + insertReg + " " + insertContr);
+			Console.WriteLine(doubleInsertInsp + " " + doubleInsertReg + " " + doubleInsertCountr);
+			Console.WriteLine(longerInsertInsp + " " + longerInsertReg + " " + longerInsertCountr);
+
+			PrintDictionary("Regions.p1.insp", regionsInsp);
+			PrintDictionary("Regions.p1.regions", regionsReg);
+			PrintDictionary("Regions.p1.country", regionsCou);
+			PrintDictionary("Regions.p2.insp", doubleRegionsInsp);
+			PrintDictionary("Regions.p2.regions", doubleRegionsReg);
+			PrintDictionary("Regions.p2.country", doubleRegionsCou);
+			PrintDictionary("Regions.p3.insp", longerRegionInsp);
+			PrintDictionary("Regions.p3.regions", longerRegionReg);
+		}
+
+		private void PrintDictionary<T>(string filename, Dictionary<string, T> dict)
+		{
+			using(var file = new StreamWriter(filename))
+			{
+				foreach(var reg in dict.Keys)
+					file.WriteLine(reg + " " + dict[reg]);
+			}
+		}
+	}
+}
+//f1027708023936 -> f1037700180462    0.0188679245283019
